c9dd89539755f7c018454a65fc7d5f8f67f7dab0
Magic-Divya <divyadiya3336@gmail.com>
2025-02-03 17:16:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Peer-Mentor Learning Series Workshop 1</h1>
         <div className="user-info">
-          <p><strong>Name:</strong> Divya</p> 
-          <p><strong>USN:</strong> 1RVU23BSC157</p>
+          <p><strong>Name:</strong> Devayani</p> 
+          <p><strong>USN:</strong> 1RVU23BSC158</p>
         </div>
       </header>
     </div>
